519b77be914eac894b1b7cc8d73ce78d308cca61
Fernanda <Fernanda@HP_Dv6>
2019-04-05 11:27:06 -0500

    Inicializacion de Proyecto

--- src/ec/edu/ups/prueba/Prueba.java
+++ src/ec/edu/ups/prueba/Prueba.java
@@ -5,7 +5,6 @@ import ec.edu.ups.clases.Carrera;
 import ec.edu.ups.clases.Grupo;
 import ec.edu.ups.clases.HistorialCalificacion;
 import ec.edu.ups.clases.Materia;
-import ec.edu.ups.clases.Persona;
 import ec.edu.ups.clases.Profesor;
 import ec.edu.ups.clases.Sede;
 public class Prueba {
